a5d24a5d17d4be69216b736914de5f8469b0150c
Patrick <patrickscool@gmail.com>
2018-08-11 22:04:44 -0700

    Removed optgroups

---
+++ countrySelector.js
@@ -0,0 +1,23 @@
+const country = $('#country');
+const regions = $('.region');
+const regionOther = $('.region[data-country=OTHER]');
+const selectedRegion = $('#selectedRegion');
+
+country.on('change', function() {
+  let activeRegion = $(`.region[data-country=${country.val()}]`);
+  if(!activeRegion.length) {
+  	activeRegion = regionOther;
+  }
+  
+  regions.removeClass('active');
+  activeRegion.addClass('active');
+  
+  updateSelectedRegion();
+})
+
+regions.on('change', updateSelectedRegion);
+
+function updateSelectedRegion() {
+  let activeRegion = $('.region.active select, .region.active input');
+  selectedRegion.val(activeRegion.val());
+}
