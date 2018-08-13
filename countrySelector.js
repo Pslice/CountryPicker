5fa2148b387be70257c89e791e1aa1926ee06b75
Patrick <patrickscool@gmail.com>
2018-08-12 22:11:48 -0700

    Added on ready

--- countrySelector.js
+++ countrySelector.js
@@ -1,8 +1,9 @@
+
+$(document).ready(function(){
 const country = $('#countryCode');
 const regions = $('.region');
 const regionOther = $('.region[data-country=OTHER]');
 const selectedRegion = $('#selectedRegion');
-
 country.on('change', function() {
   let activeRegion = $(`.region[data-country=${country.val()}]`);
   if(!activeRegion.length) {
@@ -21,3 +22,6 @@ function updateSelectedRegion() {
   let activeRegion = $('.region.active select, .region.active input');
   selectedRegion.val(activeRegion.val());
 }
+});
+
+
